bbed724ec209b3a275fd844008ef0f69e5810647
Khalil <khalilp@geekyants.com>
2024-08-12 21:43:10 +0530

    Chapter 16: Generate metadata both static and dynamic

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,12 +2,37 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata, ResolvingMetadata } from "next";
+import prisma from "@/app/lib/db";
 
-export default async function Page({
-  params: { id },
-}: {
+type Props = {
   params: { id: string };
-}) {
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id;
+  const invoice = await prisma.invoices.findUnique({
+    where: { id: id },
+    select: {
+      amount: true,
+      customer: { select: { name: true } },
+    },
+  });
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `${invoice?.customer.name} - Edit Invoice`,
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
+}
+
+export default async function Page({ params: { id } }: Props) {
   const invoice = await fetchInvoiceById(id);
   const customers = await fetchCustomers();
 
